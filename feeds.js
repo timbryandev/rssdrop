0c917076013abd876418011d6b3d7d5b1e4a60b0
timbryandev <hi@timbryan.dev>
2025-10-21 14:44:58 +0100

    Update feeds.js with TLDR

--- feeds.js
+++ feeds.js
@@ -1,4 +1,6 @@
 export const RSS_FEEDS = [
   "https://cprss.s3.amazonaws.com/javascriptweekly.com.xml",
   "https://overreacted.io/rss.xml",
+  "https://tldr.tech/api/rss/dev",
+  "https://tldr.tech/api/rss/tech",
 ];
