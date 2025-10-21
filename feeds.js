2a669392eaf9abc6f0f6f62eac2990cb71ce665a
timbryandev <hi@timbryan.dev>
2025-10-21 15:09:23 +0100

    Update feeds.js

--- feeds.js
+++ feeds.js
@@ -1,6 +1,8 @@
 export const RSS_FEEDS = [
+  "https://tldr.tech/api/rss/dev",
   "https://cprss.s3.amazonaws.com/javascriptweekly.com.xml",
   "https://overreacted.io/rss.xml",
-  "https://tldr.tech/api/rss/dev",
+  "https://hackernoon.com/tagged/programming/feed",
+  "https://dev.to/feed",
   "https://tldr.tech/api/rss/tech",
 ];
