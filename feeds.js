07e1957c38affb05c44b686e05e2a3b419051b18
timbryandev <hi@timbryan.dev>
2025-10-23 13:51:34 +0100

    Update feeds.js

--- feeds.js
+++ feeds.js
@@ -12,5 +12,6 @@ export const RSS_FEEDS = [
   "https://engineering.fb.com/feed/",
   "https://www.echojs.com/rss",
   "https://frontenddogma.com/feed/",
+  "https://hungrymindsdev.substack.com/feed",
   "https://tldr.tech/api/rss/tech",
 ];
